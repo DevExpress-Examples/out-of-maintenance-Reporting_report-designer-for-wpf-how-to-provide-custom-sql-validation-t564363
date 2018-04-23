d796f7c621ce6aca2b3cadce642e6e54181a491d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ValidateCustomSql/CustomQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.DataAccess.Wizard.Services;
+using DevExpress.DataAccess.ConnectionParameters;
+
+namespace ValidateCustomSql {
+    public class MyCustomQueryValidator : ICustomQueryValidator {
+        public bool Validate(DataConnectionParametersBase connectionParameters, string sql, ref string message) {
+            // Add your custom validation logic here. 
+            // The method should return true if the query is valid and false otherwise. 
+            return false;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Report Designer for WPF - How to provide custom SQL validation
+
+
+This example illustrates how to implement custom SQL validation to restrict execution of harmful queries when your WPF reporting application enables <a href="https://www.devexpress.com/example=T504221">entering custom SQL</a>.<br><br>In this example, you can provide a custom query validation logic either using the Services Registry or by customizing the Data Source Wizard.
+
+<br/>
+
+
